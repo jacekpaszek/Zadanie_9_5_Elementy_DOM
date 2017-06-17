5e5429849333babb05ce798053ac41f9507b09fb
Jacek <jacek.paszek@onet.pl>
2017-06-17 21:22:33 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+Projekt 9.5 Elementy DOM
+
+To jest skrypt do operacji na elementach DOM
---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+var withButtonClass = document.getElementsByClassName("button");
+var i = 0;
+
+for (i = 0; i < withButtonClass.length; i++) {
+  alert(withButtonClass.innerText);
+};
