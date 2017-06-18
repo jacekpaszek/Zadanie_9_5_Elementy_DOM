e6f818bc26f1e62c59fb12b2cea40130760fe7cb
Jacek <jacek.paszek@onet.pl>
2017-06-18 17:28:58 +0200

    loop for modification

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,5 @@
 var withButtonClass = document.getElementsByClassName("button");
-var i = 0;
 
-for (i = 0; i < withButtonClass.length; i++) {
-  alert(withButtonClass.innerText);
+for (var i = 0; i < withButtonClass.length; i++) {
+  alert(withButtonClass[i].innerText);
 };
